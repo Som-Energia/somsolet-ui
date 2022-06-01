90b6d0c7aa8fc47c88a8ae1390cc26b71c14770d
David García Garzón <david.garcia@somenergia.coop>
2022-06-01 09:29:07 +0200

    new deployment command build ov
    Cleans the target directory, moves the assets and renames them there.
    Moving code to ov was a manual and error prone operation.
    Instead of renaming files in build, renames them in target directory,
    so the same build is useful for regular and ov

--- scripts/rename-assets-ov.sh
+++ scripts/rename-assets-ov.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
 GREEN='\033[0;32m' # Green
+BLUE='\033[0;34m' # Blue
 NC='\033[0m' # No Color
+die() {
+	echo -e $RED"$*"$NC >&2
+	exit -1
+}
+step() {
+	echo -e $BLUE"$*"$NC >&2
+}
 
-jq -r '.entrypoints[]' ../build/asset-manifest.json | while read file; do
+REPOPATH=$(dirname $0)/..
+BUILD=${REPOPATH}/build
+TARGET=${REPOPATH}/../oficinavirtual/src/front/static/somsolet
+
+[ -d $(dirname $TARGET) ] || die "Target directory $(dirname $TARGET) should exist"
+
+step "Removing existing $TARGET"
+rm -rf "${TARGET}"
+mkdir "${TARGET}"
+step "Copying resources to $TARGET"
+cp -r "${BUILD}/static" "$TARGET"
+
+step "Renaming resources in $TARGET"
+
+jq -r '.entrypoints[]' ${BUILD}/asset-manifest.json | while read file; do
   if [[ $file =~ ^static/(js|css)/main\.[A-Za-z0-9]*.(js|css)$ ]]; then
     dest=$(echo ${file} | sed 's/main\.[A-Za-z0-9]*\./main./g')
     echo -e ${GREEN}✔︎${NC} ${file} → ${dest}
-    mv ../build/${file} ../build/${dest}
-    mv ../build/${file}.map ../build/${dest}.map
+    mv ${TARGET}/${file} ${TARGET}/${dest}
+    mv ${TARGET}/${file}.map ${TARGET}/${dest}.map
   fi
 
   # yarn build format
@@ -16,22 +38,22 @@ jq -r '.entrypoints[]' ../build/asset-manifest.json | while read file; do
   if [[ $file =~ ^static/js/runtime-main\.[A-Za-z0-9]*\.js$ ]]; then
     dest=$(echo ${file} | sed 's/runtime-main\.[A-Za-z0-9]*\./runtime-main./g')
     echo -e ${GREEN}✔︎${NC} ${file} → ${dest}
-    mv ../build/${file} ../build/${dest}
-    mv ../build/${file}.map ../build/${dest}.map
+    mv ${TARGET}/${file} ${TARGET}/${dest}
+    mv ${TARGET}/${file}.map ${TARGET}/${dest}.map
   fi
   
   if [[ $file =~ ^static/(js|css)/main\.[A-Za-z0-9]*\.chunk.(js|css)$ ]]; then
     dest=$(echo ${file} | sed 's/main\.[A-Za-z0-9]*\.chunk\./main./g')
     echo -e ${GREEN}✔︎${NC} ${file} → ${dest}
-    mv ../build/${file} ../build/${dest}
-    mv ../build/${file}.map ../build/${dest}.map
+    mv ${TARGET}/${file} ${TARGET}/${dest}
+    mv ${TARGET}/${file}.map ${TARGET}/${dest}.map
   fi
 
   if [[ $file =~ ^static/(js|css)/[0-9]{1,2}\.[A-Za-z0-9]*\.chunk.js$ ]]; then
     dest=$(echo ${file} | sed 's/[0-9]*\.[A-Za-z0-9]*\.chunk\./vendor./g')
     echo -e ${GREEN}✔︎${NC} ${file} → ${dest}
-    mv ../build/${file} ../build/${dest}
-    mv ../build/${file}.LICENSE.txt ../build/${dest}.LICENSE.txt
-    mv ../build/${file}.map ../build/${dest}.map
+    mv ${TARGET}/${file} ${TARGET}/${dest}
+    mv ${TARGET}/${file}.LICENSE.txt ${TARGET}/${dest}.LICENSE.txt
+    mv ${TARGET}/${file}.map ${TARGET}/${dest}.map
   fi
 done
